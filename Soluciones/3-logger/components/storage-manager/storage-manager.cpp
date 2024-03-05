b3d7d4e09d4040f6350824601701b6c8a6b52db3
vacmg <70143287+vacmg@users.noreply.github.com>
2024-03-05 13:25:24 +0100

    Finish logger tutorial

--- Soluciones/3-logger/components/storage-manager/storage-manager.cpp
+++ Soluciones/3-logger/components/storage-manager/storage-manager.cpp
@@ -1,7 +1,73 @@
-#include <stdio.h>
 #include "storage-manager.h"
+#include "esp_log.h"
 
-void func(void)
+const char* COMPONENT_TAG_STORAGE_PARTITION_MANAGER = "STORAGE_PARTITION_MANAGER";
+
+esp_err_t StoragePartitionManager::mount()
 {
+    ESP_LOGI(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Initializing '" CONFIG_STORAGE_PARTITION_LABEL "' partition");
 
+    // Use settings defined above to initialize and mount SPIFFS filesystem.
+    // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK)
+    {
+        if (ret == ESP_FAIL)
+        {
+            ESP_LOGE(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Failed to mount '" CONFIG_STORAGE_PARTITION_LABEL "' filesystem");
+        }
+        else if (ret == ESP_ERR_NOT_FOUND)
+        {
+            ESP_LOGE(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Failed to find '" CONFIG_STORAGE_PARTITION_LABEL "' SPIFFS partition");
+        }
+        else
+        {
+            ESP_LOGE(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Failed to initialize '" CONFIG_STORAGE_PARTITION_LABEL "' SPIFFS partition: %s", esp_err_to_name(ret));
+        }
+        return ret;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(conf.partition_label, &total, &used);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Failed to get '" CONFIG_STORAGE_PARTITION_LABEL "' partition information: %s", esp_err_to_name(ret));
+        return ret;
+    }
+    else
+    {
+        ESP_LOGI(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "'" CONFIG_STORAGE_PARTITION_LABEL "' partition size: total: %d, used: %d", total, used);
+    }
+
+    // Check the consistency of reported partition size info.
+    if (used > total)
+    {
+        ESP_LOGW(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Number of used bytes cannot be larger than total.");
+        return ESP_FAIL;
+    }
+    return ret;
+}
+
+esp_err_t StoragePartitionManager::format()
+{
+    ESP_LOGI(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Formatting '" CONFIG_STORAGE_PARTITION_LABEL "' partition");
+    esp_err_t ret = esp_spiffs_format(conf.partition_label);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Failed to format '" CONFIG_STORAGE_PARTITION_LABEL "' partition: %s", esp_err_to_name(ret));
+        return ret;
+    }
+    return ret;
+}
+
+esp_err_t StoragePartitionManager::unmount()
+{
+    ESP_LOGI(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Unmounting '" CONFIG_STORAGE_PARTITION_LABEL "' partition");
+    esp_err_t ret = esp_vfs_spiffs_unregister(conf.partition_label);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(COMPONENT_TAG_STORAGE_PARTITION_MANAGER, "Failed to unmount '" CONFIG_STORAGE_PARTITION_LABEL "' partition: %s", esp_err_to_name(ret));
+    }
+    return ret;
 }
--- Soluciones/3-logger/main/3-logger.cpp
+++ Soluciones/3-logger/main/3-logger.cpp
@@ -1,6 +1,80 @@
-#include <stdio.h>
+#include "esp_log.h"
+#include "storage-manager.h"
+
+const char* MAIN_TAG = "MAIN";
 
 extern "C" void app_main(void)
 {
+    StoragePartitionManager::mount();
 
+//    FILE* file = fopen("/storage/readme.txt", "r");
+//    if (file == nullptr)
+//    {
+//        ESP_LOGE(MAIN_TAG, "Failed to open file for reading");
+//    }
+//    else
+//    {
+//        char line[1024];
+//        while (fgets(line, sizeof(line), file))
+//        {
+//            printf("%s", line);
+//        }
+//        fclose(file);
+//    }
+
+    int bootNumber = 0;
+    FILE* logFile = nullptr;
+    FILE* bootNumberFile = fopen("/storage/bootNumber.txt", "r");
+    if (bootNumberFile != nullptr)
+    {
+        fscanf(bootNumberFile, "%d", &bootNumber);
+        fclose(bootNumberFile);
+        ESP_LOGI(MAIN_TAG, "Last boot number: %d", bootNumber);
+
+    }
+    else
+    {
+        ESP_LOGW(MAIN_TAG, "Failed to open /storage/bootNumber.txt for reading: creating a new file");
+        bootNumber = 0;
+    }
+
+    bootNumberFile = fopen("/storage/bootNumber.txt", "w");
+    if (bootNumberFile == nullptr)
+    {
+        ESP_LOGE(MAIN_TAG, "Failed to open /storage/bootNumber.txt for writing");
+    }
+    else
+    {
+        bootNumber++;
+        fprintf(bootNumberFile, "%d", bootNumber);
+        fclose(bootNumberFile);
+
+        logFile = fopen("/storage/log.txt", "a");
+        if (logFile == nullptr)
+        {
+            ESP_LOGE(MAIN_TAG, "Failed to open /storage/log.txt for appending");
+        }
+        else
+        {
+            fprintf(logFile, "Inicio nº %d\n", bootNumber);
+            fclose(logFile);
+        }
+    }
+
+    logFile = fopen("/storage/log.txt", "r");
+    if (logFile == nullptr)
+    {
+        ESP_LOGE(MAIN_TAG, "Failed to open /storage/log.txt for reading");
+    }
+    else
+    {
+        char line[1024];
+        while (fgets(line, sizeof(line), logFile))
+        {
+            printf("%s", line);
+        }
+        fclose(logFile);
+    }
+
+    StoragePartitionManager::unmount();
 }
